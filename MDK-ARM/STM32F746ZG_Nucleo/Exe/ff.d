0a0b3c0b6a78ab374eda5822882c7fd1f3ff68d2
MaximKolbin <42674998+MaximKolbin@users.noreply.github.com>
2025-05-19 13:45:35 +0300

    update

--- MDK-ARM/STM32F746ZG_Nucleo/Exe/ff.d
+++ MDK-ARM/STM32F746ZG_Nucleo/Exe/ff.d
@@ -1,6 +1,6 @@
-stm32f746zg_nucleo\exe\ff.o: ../../STM32F/Middlewares/Third_Party/FatFs/src/ff.c
-stm32f746zg_nucleo\exe\ff.o: ../../STM32F/Middlewares/Third_Party/FatFs/src/ff.h
-stm32f746zg_nucleo\exe\ff.o: ../../STM32F/Middlewares/Third_Party/FatFs/src/integer.h
-stm32f746zg_nucleo\exe\ff.o: ../Inc/ffconf.h
+stm32f746zg_nucleo\exe\ff.o: ..\..\..\Lib_STM32F7\Middlewares\Third_Party\FatFs\src\ff.c
+stm32f746zg_nucleo\exe\ff.o: ..\..\..\Lib_STM32F7\Middlewares\Third_Party\FatFs\src\ff.h
+stm32f746zg_nucleo\exe\ff.o: ..\..\..\Lib_STM32F7\Middlewares\Third_Party\FatFs\src\integer.h
+stm32f746zg_nucleo\exe\ff.o: ..\Inc\ffconf.h
 stm32f746zg_nucleo\exe\ff.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdlib.h
-stm32f746zg_nucleo\exe\ff.o: ../../STM32F/Middlewares/Third_Party/FatFs/src/diskio.h
+stm32f746zg_nucleo\exe\ff.o: ..\..\..\Lib_STM32F7\Middlewares\Third_Party\FatFs\src\diskio.h
